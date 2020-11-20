05c95af87ceec8f4f0a2bb6a38c3e93b6f147c50
Jeanne Legay <jeanne.legay@hotmail.fr>
2020-11-20 14:49:32 +0100

    MVC for movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_134408) do
+ActiveRecord::Schema.define(version: 2020_11_20_134655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2020_11_20_134408) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_hats_on_user_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "name"
+    t.bigint "hat_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hat_id"], name: "index_movies_on_hat_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_11_20_134408) do
   end
 
   add_foreign_key "hats", "users"
+  add_foreign_key "movies", "hats"
 end
